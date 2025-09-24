ae23d626b8cc84699c27f3089cde3f4506fbd7fc
Mehakpreet Kaur <mehakpreetkaur1805@gmail.com>
2025-09-24 09:22:38 +0530

    files added

--- middlewares/helmet.js
+++
@@ -1,8 +0,0 @@
-const helmet = require('helmet')
-
-const helmetMiddleware = helmet({
-  contentSecurityPolicy: false, 
-  crossOriginResourcePolicy: { policy: "same-origin" }, 
-})
-
-module.exports = helmetMiddleware
